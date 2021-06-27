7ab3c7cf6937bf8f69134b28cfa84aa055e4fd06
KoorunG <onlyzion10@naver.com>
2021-06-27 15:43:30 +0900

    Modify PythagoreanNumber

--- stream/PythagoreanNumber.java
+++ stream/PythagoreanNumber.java
@@ -12,7 +12,8 @@ public class PythagoreanNumber {
     .flatMap(a-> IntStream.rangeClosed(a, 100)/* 주어진 a를 이용하여 세 수의 스트림을 만듦 즉, 스트림의 스트림이 만들어지는 것이다 */
                                               /* 따라서 flatMap을 이용하여 각각의 스트림을 하나의 평준화된 스트림으로 만들어야 한다 */
                                               .filter(b -> Math.sqrt(a*a+b*b)%1 == 0)
-                                              .mapToObj(b -> new int[]{a,b,(int)Math.sqrt(a*a+b*b)}));
+                                              .mapToObj(b -> new int[]{a,b,(int)Math.sqrt(a*a+b*b)}))
+                                              .filter(t->t[2]%1==0);    // 세 수의 세번째 수는 반드시 정수이어야 한다는 조건을 추가하여 최적화 하였다
 
     public static void main(String[] args){        
     System.out.print("출력할 피타고리안 수 배열의 갯수를 입력하시오 : ");
