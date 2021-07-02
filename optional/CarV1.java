952a17a78fe50c2dcda6e83075a56441c41c2bd9
KoorunG <onlyzion10@naver.com>
2021-07-02 18:53:40 +0900

    Practice Optional

---
+++ optional/CarV1.java
@@ -0,0 +1,12 @@
+package optional;
+
+public class CarV1 {
+
+    private Insurance insurance;
+  
+    public Insurance getInsurance() {
+      return insurance;
+    }
+  
+  }
+  
---
+++ optional/Insurance.java
@@ -0,0 +1,12 @@
+package optional;
+
+public class Insurance {
+
+    private String name;
+  
+    public String getName() {
+      return name;
+    }
+    
+  }
+  
